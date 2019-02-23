47c1e59fe0b8315121b49ca1684d6e688cb95886
Silas Bates <seumasb96@gmail.com>
2019-02-22 22:03:52 -0800

    cleans up code slightly

--- reset_patch.sh
+++ reset_patch.sh
@@ -6,17 +6,19 @@ NORMAL="\033[0m"
 
 correctDir="/Users/Seumas/Library/Application Support/Steam/SteamApps/common/Borderlands 2/Borderlands2.app/Contents/GameData/Binaries"
 
+newPatch="backupPatch/Patch.txt.stable"
+
 if [ -d "${correctDir}" ]; then
   cd "${correctDir}"
 else
   exit 1
 fi
 
-newPatch="backupPatch/Patch.txt.stable"
-
 rm -i Patch.txt
 cp -vn ${newPatch} Patch.txt
 # don't put anything between these lines
+# x
+# xxx
 if [ $? -eq 0 ]; then
   echo -e "\n${GREEN}--- patch reset (Patch.txt.stable) ---${NORMAL}"
   md5 -r Patch.txt ${newPatch}
